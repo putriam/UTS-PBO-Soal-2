94521a71c214c1aec93f19b4313fdcf6c787c715
Putri Amelia <49051097+putriam@users.noreply.github.com>
2020-04-25 19:48:17 +0700

    first upload to github

---
+++ RetailItem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utspbosoal2
+{
+    class RetailItem
+    {
+        private String description;
+        private int unitsOnHand;
+        private double price;
+        // Constructor
+        public RetailItem(String description, int unitsOnHand, double price)
+        {
+            this.description = description;
+            this.unitsOnHand = unitsOnHand;
+            this.price = price;
+        }
+
+        // Accessor Method
+        public void setDescription(String description)
+        {
+            this.description = description;
+        }
+
+        public String getDescription()
+        {
+            return description;
+        }
+
+        public void setUnitsOnHand(int unitsOnHand)
+        {
+            this.unitsOnHand = unitsOnHand;
+        }
+
+        public int getUnitsOnHand()
+        {
+            return unitsOnHand;
+        }
+
+        public void setPrice(double price)
+        {
+            this.price = price;
+        }
+
+        public double getPrice()
+        {
+            return price;
+        }
+
+    }
+}
